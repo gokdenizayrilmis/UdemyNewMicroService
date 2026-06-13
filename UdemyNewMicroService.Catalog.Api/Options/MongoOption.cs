61da65fa6bf0c554bd623b189ab3ab647069b5ff
Gokdeniz <ayrilmis.gokdeniz@gmail.com>
2026-06-13 20:31:03 +0300

    added changes

---
+++ UdemyNewMicroService.Catalog.Api/Options/MongoOption.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UdemyNewMicroService.Catalog.Api.Options
+{
+    public class MongoOption
+    {
+        [Required]
+        public string DatabaseName { get; set; } = default!;
+        [Required]
+        public string ConnectionString { get; set; } = default!;
+
+    }
+}
--- UdemyNewMicroService.Catalog.Api/Program.cs
+++ UdemyNewMicroService.Catalog.Api/Program.cs
@@ -1,7 +1,12 @@
+using UdemyNewMicroService.Catalog.Api.Options;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddOptionsExt();
+
 
 var app = builder.Build();
 
