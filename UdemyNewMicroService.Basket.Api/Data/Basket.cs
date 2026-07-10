0124a78efe68cc7a9733ac44befcd25f7a7fa640
Gokdeniz <ayrilmis.gokdeniz@gmail.com>
2026-07-10 17:47:37 +0300

    baskete dataları olusturuldu

---
+++ UdemyNewMicroService.Basket.Api/Data/Basket.cs
@@ -0,0 +1,57 @@
+﻿using System.Text.Json.Serialization;
+using UdemyNewMicroService.Basket.Api.DTOs;
+
+namespace UdemyNewMicroService.Basket.Api.Data
+{
+    public class Basket
+    {
+        public Guid UserId { get; set; }
+        public List<BasketItem> Items { get; set; } = new();
+        public float? DiscountRate { get; set; }
+        public string? Coupon { get; set; }
+
+        public bool IsApplyDiscount => DiscountRate is > 0 && !string.IsNullOrEmpty(Coupon);
+        public decimal TotalPrice => Items.Sum(x => x.Price);
+        public decimal? TotalPriceWithAppliedDiscount => !IsApplyDiscount ? null : Items.Sum(x => x.PriceByApplyDiscountRate);
+        public Basket()
+        {
+            
+        }
+        public Basket(Guid userId, List<BasketItem> items)
+        {
+            UserId = userId;
+            this.Items = items;
+        }
+
+        public void ApplyNewDiscount(string coupon, float discountRate)
+        {
+            Coupon = coupon;
+            DiscountRate = discountRate;
+
+            foreach (var basket in Items)
+            {
+                basket.PriceByApplyDiscountRate = basket.Price * (decimal)(1 - discountRate);
+            }
+        }
+
+        public void ApplyAvailableDiscount()
+        {
+            foreach (var basket in Items)
+            {
+                basket.PriceByApplyDiscountRate = basket.Price * (decimal)(1 - DiscountRate!);
+            }
+        }
+
+        public void ClearDiscount()
+        {
+            Coupon = null;
+            DiscountRate = null;
+
+            foreach (var basket in Items)
+            {
+                basket.PriceByApplyDiscountRate = null;
+            }
+        }
+
+    }
+}
---
+++ UdemyNewMicroService.Basket.Api/Data/BasketItem.cs
@@ -0,0 +1,26 @@
+﻿namespace UdemyNewMicroService.Basket.Api.Data
+{
+    public class BasketItem
+    {
+        public BasketItem()
+        {
+            
+        }
+        public BasketItem(Guid ıd, string name, string? ımageUrl, decimal price, decimal? priceByApplyDiscountRate)
+        {
+            Id = ıd;
+            Name = name;
+            ImageUrl = ımageUrl;
+            Price = price;
+            PriceByApplyDiscountRate = priceByApplyDiscountRate;
+        }
+
+        public Guid Id { get; set; }
+        public string Name { get; set; } = default!;
+        public string? ImageUrl { get; set; }
+        public decimal Price { get; set; }
+        public decimal? PriceByApplyDiscountRate { get; set; }
+
+      
+    }
+}
