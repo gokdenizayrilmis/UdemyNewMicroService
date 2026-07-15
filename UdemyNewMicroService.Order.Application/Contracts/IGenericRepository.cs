d2632a9a820f92d9ce1dbd1256108c4c4cecd935
Gokdeniz <ayrilmis.gokdeniz@gmail.com>
2026-07-15 19:09:25 +0300

    genericrepository olusturuldu

---
+++ UdemyNewMicroService.Order.Application/Contracts/IGenericRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace UdemyNewMicroService.Order.Application.Contracts
+{
+    public interface IGenericRepository<TId,TEntity> where TEntity : class where TId : struct
+    {
+        public Task<bool> AnyAsync(TId id);
+        public Task<bool> AnyAsync(Expression<Func<TEntity, bool>> predicate);
+        Task<List<TEntity>> GetAllAsync();
+        Task<List<TEntity>> GetAllPagedAsync(int pageNumber, int pageSize);
+        ValueTask<TEntity> GetByIdAsync(TId id);
+        ValueTask AddAsync(TEntity entity);
+        void AddR(TEntity entity);
+        void Update(TEntity entity);
+        void Delete(TEntity entity);
+    }
+}
