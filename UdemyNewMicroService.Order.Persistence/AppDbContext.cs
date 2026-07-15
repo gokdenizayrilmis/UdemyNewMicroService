e97b5579f479d7e80c5e769129abc68b6fb372c2
Gokdeniz <ayrilmis.gokdeniz@gmail.com>
2026-07-15 17:15:03 +0300

    dbcontext yazildi

---
+++ UdemyNewMicroService.Order.Persistence/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UdemyNewMicroService.Order.Domain.Entities;
+using UdemyNewMicroService.Order.Persistence.Configurations;
+
+namespace UdemyNewMicroService.Order.Persistence
+{
+    public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+    {
+        DbSet<Domain.Entities.Order> Orders { get; set; }
+        DbSet<OrderItem> OrderItems { get; set; }
+        DbSet<Address> Addresses { get; set; }
+        override protected void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(PersistenceAssembly).Assembly);
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ UdemyNewMicroService.Order.Persistence/PersistenceAssembly.cs
@@ -0,0 +1,3 @@
+﻿namespace UdemyNewMicroService.Order.Persistence;
+    public struct PersistenceAssembly;
+
